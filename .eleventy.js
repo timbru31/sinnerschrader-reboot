cc48928eef8959969e90fd7b1f5264f9688b0f34
Tim Brust <tim.brust@sinnerschrader.com>
2021-02-01 17:29:15 +0100

    refactor: move layout to _includes folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,3 @@
 module.exports = function (eleventyConfig) {
 	eleventyConfig.setTemplateFormats(["liquid"]);
-
-	return {
-		dir: {
-			includes: "_includes",
-			layouts: "_layouts",
-		},
-	};
 };
