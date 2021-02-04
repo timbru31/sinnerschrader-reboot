08a2cfd3d2747a4c7d9fab99c560d5afcc5cc3cc
Tim Brust <github@timbrust.de>
2021-02-04 17:46:18 +0100

    build: adds scss and postcss support in build pipeline (#5)

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,22 @@
-module.exports = function (eleventyConfig) {
-	eleventyConfig.setTemplateFormats(["liquid"]);
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const { default: postcss } = require("postcss");
+const processSassFiles = require("./config/process-sass");
+
+module.exports = (eleventyConfig) => {
+	processSassFiles("./styles/index.scss", "./_includes/css/main.css");
+
+	eleventyConfig.setTemplateFormats(["liquid", "njk"]);
+
+	eleventyConfig.addTransform("async-transform-name", async (content, outputPath) => {
+		if (outputPath.endsWith(".css")) {
+			const postCSSResult = await postcss([autoprefixer, cssnano]).process(content, { from: outputPath, to: outputPath });
+			return postCSSResult.css;
+		}
+		return content;
+	});
+
+	eleventyConfig.setBrowserSyncConfig({
+		files: "./_site/stylesheet.css",
+	});
 };
---
+++ config/process-sass.js
@@ -0,0 +1,37 @@
+// Based on https://dev.to/mathieuhuot/processing-sass-with-11ty-5a09
+const { dirname, join } = require("path");
+const { writeFile, watch, utimes } = require("fs-extra");
+const fibers = require("fibers");
+const sass = require("sass");
+
+const renderSassAndSave = (src, dest) => {
+	return new Promise((resolve, reject) => {
+		sass.render(
+			{
+				file: src,
+				fiber: fibers,
+			},
+			(error, result) => {
+				if (error) {
+					reject(error);
+				}
+				resolve(result);
+				console.log(`Writing ${dest} from ${result.stats.entry} in ${result.stats.duration}ms.`);
+			}
+		);
+	})
+		.then((result) => writeFile(dest, result.css.toString()))
+		.then((_) => {
+			const time = new Date();
+			return utimes(join(process.cwd(), "pages/stylesheet.njk"), time, time);
+		})
+		.catch(console.error);
+};
+
+module.exports = (scssPath, cssPath) => {
+	renderSassAndSave(scssPath, cssPath);
+
+	if (process.argv.includes("--serve")) {
+		watch(dirname(scssPath), () => renderSassAndSave(scssPath, cssPath));
+	}
+};
