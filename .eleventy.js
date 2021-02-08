a2084798ce2655b7e1bde68889bd5d97fd6bdfa8
Tamara Kis <kis.tamara91@gmail.com>
2021-02-08 14:27:28 +0100

    Feat/scss setup (#6)
    Co-authored-by: tamara.kis <tamara.kis@sinnerschrader.com>
    Co-authored-by: Tim Brust <github@timbrust.de>

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,19 @@
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
+const chokidar = require("chokidar");
+const { utimes } = require("fs-extra");
+const htmlmin = require("html-minifier");
+const { join } = require("path");
 const { default: postcss } = require("postcss");
+
 const processSassFiles = require("./config/process-sass");
 
-module.exports = (eleventyConfig) => {
+module.exports = eleventyConfig => {
+	const watcher = chokidar.watch("styles/{includes,core}/*.scss", {
+		ignored: /(^|[\/\\])\../,
+		persistent: true
+	});
+	watcher.on("add", touchFile).on("change", touchFile);
 	processSassFiles("./styles/index.scss", "./_includes/css/main.css");
 
 	eleventyConfig.setTemplateFormats(["liquid", "njk"]);
@@ -17,6 +27,27 @@ module.exports = (eleventyConfig) => {
 	});
 
 	eleventyConfig.setBrowserSyncConfig({
-		files: "./_site/stylesheet.css",
+		files: "./_site/stylesheet.css"
+	});
+
+	eleventyConfig.addWatchTarget("_site/index.css");
+	eleventyConfig.setBrowserSyncConfig({ files: ["_site/index.css"] });
+
+	eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+		if (outputPath.endsWith(".html")) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true
+			});
+			return minified;
+		}
+
+		return content;
 	});
 };
+
+function touchFile() {
+	const time = new Date();
+	utimes(join(process.cwd(), "styles/index.scss"), time, time);
+}
