68ebc4e0eae25eb448c83c18fd0d288310d4a519
David Zimmermann <zimmermann.d90@gmail.com>
2021-02-22 11:29:21 +0100

    Feat/logo section (#44)
    * feature(video): add video and logo section
    
    * fix(slider): change visible slider items & grid gaps, fix overflow, remove rotating circle text
    
    * fix stylelint
    
    * feat(video): customize player, adjust behaviour on mobile
    
    * finalizing
    
    * small fixes
    
    * feat(video): fix mobile play/pause, fix no detection of end position, add responsive styles
    
    * fix(stylelint): disable alphabetical order, fix other stylelint errors
    
    * fix(work-section): exchange px with rem
    
    Co-authored-by: David Zimmermann <david.zimmermann@sinnerschrader.com>

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,7 +1,9 @@
 import { Slider } from "./slider.js";
 import { Locations } from "./locations";
+import { Video } from "./video.js";
 
 (() => {
 	new Locations();
 	new Slider();
+	new Video();
 })();
---
+++ _includes/js/video.js
@@ -0,0 +1,55 @@
+class Video {
+	constructor() {
+		this.init();
+	}
+
+	init() {
+		this.video = {
+			previewElement: document.querySelector(".work__video-preview-loop"),
+			playButton: document.querySelector(".work__video-media-control"),
+			videoElement: document.querySelector(".work__video-element"),
+		};
+
+		this.bindEvents();
+	}
+
+	bindEvents() {
+		this.video.playButton.addEventListener("click", this.togglePlay.bind(this));
+		this.video.videoElement.addEventListener("ended", this.toggleBtn.bind(this));
+	}
+
+	togglePlay() {
+		const videoPreloadAttr = this.video.videoElement.getAttribute("preload");
+
+		if (videoPreloadAttr === "none") {
+			this.displayVideo();
+		}
+
+		this.video.videoElement.paused === true ? this.video.videoElement.play() : this.video.videoElement.pause();
+		this.toggleBtn();
+	}
+
+	toggleBtn() {
+		this.video.playButton.classList.toggle("is-playing");
+	}
+
+	displayVideo() {
+		this.video.previewElement.classList.add("is-hidden");
+		this.video.videoElement.classList.add("is-visible");
+		this.video.videoElement.setAttribute("preload", "auto");
+
+		if (this.isTouchDevice()) {
+			this.fullscreenOnMobile();
+		}
+	}
+
+	fullscreenOnMobile() {
+		this.video.videoElement.requestFullscreen().then(() => this.video.videoElement.play());
+	}
+
+	isTouchDevice() {
+		return "ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+	}
+}
+
+export { Video };
