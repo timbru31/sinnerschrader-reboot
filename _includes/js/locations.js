e744ddece1fbcbebb3e58f7ebcc463b665854c98
David Zimmermann <zimmermann.d90@gmail.com>
2021-03-12 17:39:17 +0100

    refactor(locations): complete refactor (#154)
    * refactor(locations): complete refactor
    
    * refactor(locations): add image delay, remove red focus outline
    
    * fix(locations): bullet circle font and image placement mobile
    
    * feat(locations): fix overflow of left picture on bp-xs
    
    * fix(stylelint): make it happy
    
    Co-authored-by: David Zimmermann <david.zimmermann@sinnerschrader.com>
    Co-authored-by: tamara.kis <tamara.kis@sinnerschrader.com>

--- _includes/js/locations.js
+++ _includes/js/locations.js
@@ -1,144 +1,62 @@
 export class Locations {
-	data = {};
-
-	BUTTON_NAME = "locations__navigation-button";
-	CONTENT_NAME = "locations__content";
-
-	CONTENT_HIDDEN_CLASS = this.CONTENT_NAME + "--hidden";
-	BUTTON_SELECTED_CLASS = this.BUTTON_NAME + "--selected";
-
-	locationContainers = [];
-	locationButtons = [];
-	locationIndicator;
-
-	timeouts = [];
-
 	constructor() {
-		fetch("./data/locations.json")
-			.then((response) => response.json())
-			.then((json) => this.init(json))
-			.catch((error) => console.error("Couldn't fetch locations data: " + error));
+		this.container = document.querySelector(".locations");
+		this.tablist = this.container.querySelector("[role=tablist]");
+		this.tabs = this.container.querySelectorAll("[role=tab]");
+		this.tabpanels = this.container.querySelectorAll("[role=tabpanel]");
+		this.activeTab = this.container.querySelector("[role=tab][aria-selected=true]");
+
+		this.bindEvents();
 	}
 
-	init = (jsonData) => {
-		this.data = jsonData;
-
-		this.loadLocationContainers();
-		this.loadLocationButtons();
-		this.loadLocationIndicator();
-		this.renderSelectedLocation(this.data[0].name);
-	};
-
-	linkId = (locationName) => `${this.BUTTON_NAME}-${locationName}`;
-	contentClassName = (locationName) => `${this.CONTENT_NAME}-${locationName}`;
-
-	loadLocationIndicator = () => {
-		this.locationIndicator = document.getElementById("locations__navigation-indicator");
-	};
-
-	loadLocationButtons = () => {
-		this.locationButtons = this.data.map((location) => {
-			const link = document.getElementById(this.linkId(location.id));
-
-			if (link) {
-				link.addEventListener("click", () => this.renderSelectedLocation(location.id));
-			}
-
-			return link;
-		});
-	};
-
-	loadLocationContainers = () => {
-		this.locationContainers = this.data
-			.map((location) => document.getElementsByClassName(this.contentClassName(location.id)))
-			.reduce((acc, current) => [...acc, ...current], []);
-	};
-
-	viewPortByMediaQuery = (query) => {
-		switch (query) {
-			case "(max-width: 599px)":
-				return "mobile";
-			case "(min-width: 600px) and (max-width: 1199px)":
-				return "tablet";
-			case "(min-width: 1200px)":
-			default:
-				return "desktop";
-		}
-	};
-
-	renderLocationButtons = (newButtonId) => {
-		this.locationButtons.forEach((button) => {
-			if (!button) return;
-
-			const isSelected = button.id === newButtonId;
-
-			if (isSelected) {
-				this.locationIndicator.style.top = button.offsetTop + button.clientHeight / 2 + "px";
-				button.classList.add(this.BUTTON_SELECTED_CLASS);
-			} else {
-				button.classList.remove(this.BUTTON_SELECTED_CLASS);
-			}
-		});
-	};
-
-	setPictureSrc = (container, isShown, { imageLarge, imageSmall, imageFallbackLarge, imageFallbackSmall }) => {
-		const picture = container.querySelector("picture");
-		if (!picture) {
-			return;
-		}
-
-		const sources = picture.querySelectorAll("source");
-		const img = picture.querySelector("img");
-
-		if (isShown && !img.getAttribute("src")) {
-			const isBigImage = container.classList.contains("locations__image-wrapper-large");
-			const imageName = isBigImage ? imageLarge : imageSmall;
-			const fallbackName = isBigImage ? imageFallbackLarge : imageFallbackSmall;
-
-			img.setAttribute("src", `./assets/images/fallback/${fallbackName}`);
-
-			sources.forEach((source) => {
-				const viewport = this.viewPortByMediaQuery(source.media);
-				source.setAttribute("srcset", `./assets/images/${viewport}/${imageName}`);
+	bindEvents() {
+		for (let tab of this.tabs) {
+			tab.addEventListener("click", (e) => {
+				e.preventDefault();
+				this.selectLocationHandler(tab.getAttribute("aria-controls"));
+			});
+			tab.addEventListener("keyup", (e) => {
+				e.preventDefault();
+				if (e.code === "Enter") {
+					this.selectLocationHandler(tab.getAttribute("aria-controls"), e);
+				}
 			});
 		}
-	};
+	}
 
-	renderLocationContainers = (newLocationData) => {
-		this.timeouts = this.timeouts.map(clearTimeout);
+	selectLocationHandler(id, e) {
+		this.setActiveControlTab(id);
+		this.setActivePanel(id, e);
+	}
 
-		this.locationContainers.forEach((container) => {
-			if (!container) return;
-
-			const shownClassName = this.contentClassName(newLocationData.id);
-			const isShown = container.classList.contains(shownClassName);
-
-			this.setPictureSrc(container, isShown, newLocationData);
-
-			let animationCallback;
-			if (isShown) {
-				container.style.display = "block";
-
-				animationCallback = () => {
-					container.classList.remove(this.CONTENT_HIDDEN_CLASS);
-				};
+	setActiveControlTab(id) {
+		for (let tab of this.tabs) {
+			if (tab.getAttribute("aria-controls") === id) {
+				tab.setAttribute("aria-selected", "true");
+				this.activeTab = tab;
 			} else {
-				container.classList.add(this.CONTENT_HIDDEN_CLASS);
-
-				animationCallback = () => {
-					container.style.display = "none";
-				};
+				tab.setAttribute("aria-selected", "false");
 			}
+		}
+	}
 
-			this.timeouts.push(setTimeout(animationCallback, 250));
-		});
-	};
+	setActivePanel(id, e) {
+		for (let tabpanel of this.tabpanels) {
+			if (tabpanel.getAttribute("id") === id) {
+				tabpanel.setAttribute("aria-expanded", "true");
 
-	renderSelectedLocation = (newName) => {
-		const newButtonId = this.linkId(newName).toLowerCase();
-		this.renderLocationButtons(newButtonId);
+				if (e !== undefined && e.code === "Enter") {
+					this.setTabpanelFocus(tabpanel);
+				}
+			} else {
+				tabpanel.setAttribute("aria-expanded", "false");
+			}
+		}
+	}
 
-		const newLocationData = this.data.find((location) => location.id === newName.toLowerCase());
-		this.renderLocationContainers(newLocationData);
-	};
+	setTabpanelFocus(panel) {
+		const focusableElements = panel.querySelectorAll("a");
+
+		focusableElements[0].focus();
+	}
 }
